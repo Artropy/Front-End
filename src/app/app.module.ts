cae27a48e990c0fe99957d560e08a87c24d366ea
Stephen Moreira <smoreira1@live.com>
2017-11-15 16:46:49 -0500

    massive revamp on going

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 
 
 // Angular Material Imports
@@ -11,13 +13,19 @@ import { MatButtonModule } from '@angular/material';
 import { MatFormFieldModule } from '@angular/material';
 import { MatInputModule } from '@angular/material';
 import {MatExpansionModule} from '@angular/material';
+import {MatListModule} from '@angular/material';
+import {MatCardModule} from '@angular/material';
+import {MatGridListModule} from '@angular/material';
+
+
 
 
 
 
 // Artropy custom components
 import { ArtropyTopNavComponent } from './top-nav/top-nav.component';
-import { ArtropySideNavComponent } from './side-nav/side-nav.component';
+// import { ArtropySideNavComponent } from './side-nav/side-nav.component';
+import { ArtropyHomePageComponent } from './home-page/home-page.component';
 
 import { MatSidenavModule } from '@angular/material';
 
@@ -26,16 +34,28 @@ import { AppComponent } from './app.component';
 
 
 
-// const appRoutes: Routes = [
-// {path: '', component}
-// ];
+const appRoutes: Routes = [
+  { path: 'home', component: ArtropyHomePageComponent },
+  // { path: 'Studios', component: ArtropyHomePageComponent },
+  // { path: 'Reimaginations', component: ArtropyReimaginationsComponent },
+  // { path: 'Trending', component: ArtropyHomePageComponent },
+  // { path: 'Masterworks', component: ArtropyHomePageComponent },
+  // { path: 'Shop', component: ArtropyHomePageComponent },
+  // { path: 'History', component: ArtropyHomePageComponent },
+  // { path: 'Liked', component: ArtropyHomePageComponent },
+  // { path: 'Following', component: ArtropyFollowingComponent },
+  // { path: 'Account', component: ArtropyFollowingComponent },
+  // { path: 'Login', component: ArtropyFollowingComponent },
+  // { path: 'Create', component: ArtropyFollowingComponent },
+  // { path: 'Search', component: ArtropyFollowingComponent },
 
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ArtropyTopNavComponent,
-    ArtropySideNavComponent
+    ArtropyHomePageComponent
   ],
   imports: [
     BrowserModule,
@@ -46,10 +66,19 @@ import { AppComponent } from './app.component';
     MatInputModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatListModule,
+    HttpClientModule,
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    MatCardModule,
+    MatGridListModule
 
   ],
   providers: [],
-  bootstrap: [AppComponent, ArtropyTopNavComponent, ArtropySideNavComponent]
+  bootstrap: [AppComponent, ArtropyTopNavComponent, ArtropyHomePageComponent]
 })
 export class AppModule { }
---
+++ src/app/liked-page/home-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { MatToolbarModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material';
+import { MatExpansionModule } from '@angular/material';
+
+
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: 'home-page.html',
+  styleUrls: ['home-page.css']
+})
+export class ArtropyHomePageComponent { }
