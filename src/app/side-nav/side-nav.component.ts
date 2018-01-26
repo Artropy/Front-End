c5227e20e4369de4bb01c07c81f54418af8faa83
Stephen Moreira <smoreira1@live.com>
2018-01-26 15:21:53 -0500

    further css3 grid changes

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SideNavComponent implements OnInit {
 
