c64667cfed5f8368c4382ae5303bab7309b0af18
Stephen Moreira <smoreira1@live.com>
2018-04-19 15:40:05 -0400

    hot fix

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -14,40 +14,40 @@ export class LoginPageComponent implements OnInit {
   }
 
 
- private checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      console.log(response);
-      if (response.status === 'connected') {
-        // the user is logged in and has authenticated your
-        // app, and response.authResponse supplies
-        // the user's ID, a valid access token, anosigned
-        // request, and the time the access token 
-        // and signed request each expire
-        let uid = response.authResponse.userID;
-        let accessToken = response.authResponse.accessToken;
-      } else if (response.status === 'not_authorized') {
-        // the user must go through the login flow
-        // to authorize your app or renew authorization
-        this.facebookLogin();
-      } else {
-        // the user isn't logged in to Facebook.
-      }
-    });
-  };
+//  private checkLoginState() {
+//     FB.getLoginStatus(function(response) {
+//       console.log(response);
+//       if (response.status === 'connected') {
+//         // the user is logged in and has authenticated your
+//         // app, and response.authResponse supplies
+//         // the user's ID, a valid access token, anosigned
+//         // request, and the time the access token 
+//         // and signed request each expire
+//         let uid = response.authResponse.userID;
+//         let accessToken = response.authResponse.accessToken;
+//       } else if (response.status === 'not_authorized') {
+//         // the user must go through the login flow
+//         // to authorize your app or renew authorization
+//         this.facebookLogin();
+//       } else {
+//         // the user isn't logged in to Facebook.
+//       }
+//     });
+//   };
 
 
-  function facebookLogin() {
-    FB.login(function(response) {
-      if (response.authResponse) {
-       console.log('Welcome!  Fetching your information.... ');
-       FB.api('/me', function(response) {
-         console.log('Good to see you, ' + response.name + '.');
-       });
-      } else {
-       console.log('User cancelled login or did not fully authorize.');
-      }
-    });
-  };
+  // function facebookLogin() {
+  //   FB.login(function(response) {
+  //     if (response.authResponse) {
+  //      console.log('Welcome!  Fetching your information.... ');
+  //      FB.api('/me', function(response) {
+  //        console.log('Good to see you, ' + response.name + '.');
+  //      });
+  //     } else {
+  //      console.log('User cancelled login or did not fully authorize.');
+  //     }
+  //   });
+  // };
 
 
 
