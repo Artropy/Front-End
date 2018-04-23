2f2fe31e021816309100f64869c92b55753304ce
Stephen Moreira <smoreira1@live.com>
2018-04-23 13:22:44 -0400

    changes to liked page format

--- src/app/liked-page/liked-page.component.ts
+++ src/app/liked-page/liked-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-liked-page',
   templateUrl: './liked-page.component.html',
-  styleUrls: ['./liked-page.component.css'],
+  styleUrls: ['./liked-page.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class LikedPageComponent implements OnInit {
